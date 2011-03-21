7ff27af58a7871ced4448ae10d2a3cf6b587d75e
Holger Just <hjust@meine-er.de>
2011-03-21 22:10:39 +0100

    Fix whitespace

--- xenapi.rb
+++ xenapi.rb
@@ -1,6 +1,6 @@
 # ===========================================================================
 # Copyright (c) 2010 Holger Just
-# 
+#
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
 # files (the "Software"), to deal in the Software without
@@ -9,10 +9,10 @@
 # copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following
 # conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -35,9 +35,9 @@ require 'xmlrpc/client'
 module XenAPI
   API_VERSION_1_1 = '1.1'
   API_VERSION_1_2 = '1.2'
-  
+
   RETRY_COUNT = 3
-  
+
   class SessionInvalidError < Exception; end
   class Failure < Exception
     def initialize(details = [])
@@ -48,17 +48,17 @@ module XenAPI
         @error_details = []
       end
     end
-    
+
     def to_s
       details = case @error_details.length
         when 0 then ""
         when 1 then @error_details[0]
         else @error_details.inspect
       end
-      
+
       "#{@error_type.to_s}: #{details}"
     end
-    
+
     attr_reader :error_type, :error_details
   end
 
@@ -70,7 +70,7 @@ module XenAPI
       #  * "socket:///var/xapi/xapi"
       # proxy_host and proxy_port can be used to specify an HTTP proxy
       @uri = URI.parse(uri)
-      
+
       case @uri.scheme.downcase
       when 'http', 'https'
         super(
@@ -88,13 +88,13 @@ module XenAPI
       else
         raise ArgumentError.new("Unknown scheme")
       end
-      
+
       @api_version = API_VERSION_1_1
       @session = ""
     end
-    
+
     attr_reader :uri, :api_version
-    
+
     LOGIN_METHODS = %w(login_with_password slave_local_login_with_password)
     LOGIN_METHODS.each do |method|
       class_eval <<-"END_EVAL", __FILE__, __LINE__
@@ -105,7 +105,7 @@ module XenAPI
             raise ::XMLRPC::FaultException.new(500,
               'Received SESSION_INVALID when logging in')
           end
-          
+
           @session = result
           @last_login_method = :#{method}
           @last_login_params = args
@@ -113,7 +113,7 @@ module XenAPI
         end
       END_EVAL
     end
-    
+
     def logout
       # preferred method to logout the session
       if @last_login_method.to_s.start_with?("slave_local")
@@ -122,7 +122,7 @@ module XenAPI
         self.session.logout
       end
     end
-    
+
     def proxy(prefix=nil, *args)
       # Overrides base method to use our custom Proxy class
       XenAPIProxy.new(self, prefix, args, :call)
@@ -132,11 +132,11 @@ module XenAPI
       # Overrides base method to use our custom Proxy class
       XenAPIProxy.new(self, prefix, args, :call2)
     end
-    
+
     def method_missing(sym, *args)
       self.proxy(sym.to_s, *args)
     end
-    
+
   private
     def _api_version()
       pool = self.pool.get_all()[0]
@@ -145,7 +145,7 @@ module XenAPI
       minor = self.host.get_API_version_minor(host)
       "#{major}.#{minor}"
     end
-    
+
     def _logout
       # called from proxy object to release all session state
       @session = ""
@@ -154,7 +154,7 @@ module XenAPI
       @api_version = API_VERSION_1_1
     end
   end
-  
+
   class XenAPIProxy < ::XMLRPC::Client::Proxy
     def method_missing(method, *args, &block)
       begin
@@ -175,7 +175,7 @@ module XenAPI
               end
             end
           end
-          
+
           raise ::XMLRPC::FaultException.new(500,
             "Tried #{RETRY_COUNT} times to get a valid session, but failed")
         end
@@ -184,7 +184,7 @@ module XenAPI
         @server.send(:_logout) if (@prefix == 'session.') && (method == :logout)
       end
     end
-    
+
     # method name clash between built-in clone and the method to clone a VM
     def clone(*args)
       args.length > 0 ? method_missing(:clone, *args) : super
@@ -195,12 +195,12 @@ module XenAPI
       # returns an instance variable of the server
       @server.instance_variable_get("@#{arg.to_s}")
     end
-    
+
     def parse_result(result)
       unless result.is_a?(Hash) && result.include?('Status')
         raise XMLRPCClientError.new('Missing Status in response from server: ' + result.inspect)
       end
-      
+
       if result['Status'] == 'Success'
         if result.include? 'Value'
           result['Value']
